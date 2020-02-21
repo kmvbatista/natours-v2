5af263ad032007059d488e8ede36848a1f6b0f2c
Kennedy <kennedymessias.vb@gmail.com>
2020-02-21 13:48:50 -0300

    tour service

---
+++ services/tourService.js
@@ -0,0 +1,13 @@
+exports.getAll = (req, res) => {
+  console.log('aaaaaa');
+};
+
+exports.getById = (req, res) => {
+  console.log('bbbbbb');
+};
+
+exports.deleteById = (req, res) => {};
+
+exports.create = (req, res) => {};
+
+exports.update = (req, res) => {};
