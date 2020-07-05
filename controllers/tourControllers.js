886b08b48f8a46831ac2afd9ce7da70dfc335f5b
Kennedy <kennedymessias.vb@gmail.com>
2020-07-05 15:19:11 -0300

    routes changes

--- controllers/tourControllers.js
+++ controllers/tourControllers.js
@@ -1,20 +1,17 @@
 const express = require('express');
 const tourService = require('../services/tourService');
-
-app.use('/api/v1/tours', router);
-
 const router = express.Router();
 
 router.param('id', tourService.checkId);
 
 router
   .route('/')
-  .getById(tourService.getById)
+  .get(tourService.getAll)
   .post(tourService.checkBody, tourService.create);
 
 router
   .route('/:id')
-  .get(tourService.getAll)
+  .get(tourService.getById)
   .patch(tourService.update)
   .delete(tourService.deleteById);
 
