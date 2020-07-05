d83ba0ee4197b44d47214033b6629f35a0f381e9
Kennedy <kennedymessias.vb@gmail.com>
2020-07-05 15:19:53 -0300

    setting up database connections

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
-import tourRouter from './controllers/tourControllers';
+const tourRouter = require('./controllers/tourControllers');
 
 const app = express();
 
-app.use(morgan('dev'));
+if (process.env.NODE_ENV == 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use('/api/v1/users', userRouter);
 app.use('/api/v1/tours', tourRouter);
 
 module.exports = app;
